7ae0f078edf40aa8c31b73ae8438af6ae9fe6bbd
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-11-30 19:15:47 +0000

    fix speaker info and test script for new plot

--- script.R
+++ script.R
@@ -1,15 +1,31 @@
 library(readr)
 library(leaflet)
 library(tidygeocoder)
+library(ggplot2)
 
-speaker_data <- read.csv(file = "speakers/speakers.csv", comment.char = "#")
+# import data ----
 
-event_data <- read.csv(file = "events/events.csv", comment.char = "#")
+speaker_data <- read_csv(file = "speakers/speakers.csv", comment = "#", show_col_types = FALSE)
 
-lat_longs <- event_data %>%
-  geocode(city = city, method = 'osm')
+event_data <- read_csv(file = "events/events.csv", comment = "#", show_col_types = FALSE)
+
+# get lat/long manually and save to avoid waiting each time
+# lat_longs <- event_data %>%
+#   geocode(city = city, method = 'osm')
+
+# location of events ----
 
 leaflet() %>%
   setView(lng = 2.3488, lat = 48.85341, zoom = 4) %>% # Paris: 48.85341 2.3488
   addTiles() %>%
   addMarkers(~long, ~lat, label = ~as.character(city), data = lat_longs)
+
+# proportion of speakers from each country ----
+
+ggplot(speaker_data) +
+  geom_bar(aes(country, fill = country)) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+    axis.title = element_blank()
+  )
