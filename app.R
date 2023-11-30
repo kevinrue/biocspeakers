38a1ccdf88e2495ab968612759b63d0dcd159444
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-11-30 09:34:46 +0000

    initialise app; display table of speakers

---
+++ app.R
@@ -0,0 +1,32 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+library(readr)
+
+# Speaker data ----
+
+speaker_data <- read.csv(file = "speakers.csv", comment.char = "#")
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Speakers stats"),
+  dashboardSidebar(),
+  dashboardBody(
+    # fluidRow(
+    #   column(width = 12, dataTableOutput("speaker_data_table"))
+    # )
+    box(
+      dataTableOutput("speaker_data_table"),
+      title = "Table",
+      width = 12
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$speaker_data_table <- renderDataTable(speaker_data)
+}
+
+app <- shinyApp(ui, server)
+
+shiny::runApp(app, launch.browser = TRUE)
